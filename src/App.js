98e19096b988fd0cc730a5e0c7ec9e4e52ec3ff0
allison <allison.villa@outlook.com>
2022-03-16 16:14:33 -0400

    moved api call and filter logic to form component, redirect to game page when game found

--- src/App.js
+++ src/App.js
@@ -1,132 +1,10 @@
 import './App.css';
-import { useEffect, useState, useRef } from 'react'; 
-import { Routes, Route, Link } from 'react-router-dom';
-import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
 // Import components
-import Form from './Form'; 
+import Form from './Form';
 import GamePage from './GamePage';
 
 function App() {
-    // Stateful variable to hold the game that will be displayed to the user
-    const [suggestedGame, setSuggestedGame] = useState({});
-
-    // Stateful variables for the user's choices from the form component
-    const [userPlatform, setUserPlatform] = useState('');
-    const [userGenre, setUserGenre] = useState('');
-    const [userTags, setUserTags] = useState([]);
-
-    // Use useRef to check if it is the page's initial render - if it is, do not make the API call
-    const initialRender = useRef(true);
-
-    // A function that is passed as props to the Form component and updates the stateful variables holding the user's choices
-    const userSelect = (chosenPlatform, chosenGenre, chosenTags) => {
-        setUserPlatform(chosenPlatform);
-        setUserGenre(chosenGenre);
-        setUserTags(chosenTags);
-    };
-
-    // A function that filters through the data returned by the API and returns games that match the user's parameters
-    const gameFinder = (gamesArray) => {
-        // An array that will hold the results from the first filter (genre)
-        const firstFilter = [];
-        // An array that will hold the final filtered results
-        const finalResults = [];
-
-        // Filter the results for games that match the user's chosen genre
-        gamesArray.forEach((game) => {
-            game.genres.forEach((genre) => {
-                let genreName = genre.slug;
-
-                if (genreName.includes(userGenre)) {
-                    firstFilter.push(game);
-                }
-            });
-        });
-
-        // Filter the results for games that match all of the user's chosen tags
-        firstFilter.forEach((game) => {
-            // An array that will hold the tags for each game so that we can compare them to userTags
-            const tagArray = [];
-
-            // A function that takes two arrays as parameters and checks that targetArray includes every value in checkedArray - returns a boolean value
-            const matchChecker = (targetArray, checkedArray) =>
-                checkedArray.every((value) => targetArray.includes(value));
-
-            game.tags.forEach((tag) => {
-                tagArray.push(tag.slug);
-
-                let isMatch = matchChecker(tagArray, userTags);
-
-                if (isMatch && finalResults.includes(game) === false) {
-                    finalResults.push(game);
-                    console.log('Found some matching tags - your filter is still working');
-                }
-            });
-        });
-        // After filtering, randomize a result and set it as suggestedGame
-        const arrayRandomizer = (array) => {
-            const arrayIndex = Math.floor(Math.random() * array.length);
-            return array[arrayIndex];
-        };
-        setSuggestedGame(arrayRandomizer(finalResults));
-    };
-
-    // Using useEffect, make an API call once the user has submitted the form
-    useEffect(() => {
-        const apiKey = `92bb52f637714b219136e934ac1b2969`;
-
-        // An array that will hold the total contents of each API call
-        const apiArray = [];
-        // A variable that will be used to check if the next page of API data exists
-        let isNextNull = false;
-
-        // An asynchronous function that will accept a page number as a parameter and make a request to the API using that page number
-        async function getPageOfResults(page) {
-            await axios
-                .get(
-                    `https://api.rawg.io/api/games?key=${apiKey}&page_size=125&ordering=-metacritic&genres=indie&tags=${userTags}&platforms=${userPlatform}&page=${page}`
-                )
-                .then((apiData) => {
-                    if (apiData.data.next == null) {
-                        isNextNull = true;
-                    }
-                    apiData.data.results.forEach((gameObject) => {
-                        apiArray.push(gameObject);
-                    });
-                });
-        }
-
-        // An asynchronous function that will keep track of the current page of API data, whether the next page exists, and make a request for the next page using getPageOfResults() if it does exist
-        async function getResults() {
-            let page = 1;
-            while (apiArray.length < 120 && isNextNull === false) {
-                await getPageOfResults(page);
-                page++;
-            }
-            // Call gameFinder() on the apiArray to filter through the results
-            gameFinder(apiArray);
-        }
-
-        if (initialRender.current) {
-            // If it is the initial render, set initialRender to false
-            initialRender.current = false;
-        } else {
-            // If it isn't the initial render, make the API call
-            getResults();
-        }
-    // Run this side effect when the user submits the form
-    }, [userTags, userPlatform]);
-
-    // Temporary results component
-    const Results = () => {
-        return (
-            <>
-                <h2>You should play {suggestedGame.name}</h2>
-                <p>Find out more <Link to={`/${suggestedGame.id}`}>here</Link></p>
-            </>
-        );
-    }
-
     return (
         <div className="App">
             <div className="wrapper">
@@ -137,18 +15,8 @@ function App() {
                     </div>
                 </header>
 
-                <Results />
-
                 <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <Form
-                                handleSubmit={userSelect}
-                                currentGameId={suggestedGame.id} 
-                            />
-                        }
-                    />
+                    <Route path="/" element={<Form />} />
                     <Route path="/:gameId" element={<GamePage />} />
                 </Routes>
             </div>
--- src/Form.js
+++ src/Form.js
@@ -1,27 +1,128 @@
-import {useState, useRef} from 'react'; 
+import {useState, useEffect, useRef} from 'react'; 
 import { Navigate } from 'react-router-dom';
+import axios from 'axios';
 
-const Form = (props) => {
-    const [platformValue, setPlatformValue] = useState(''); 
+const Form = () => {
+    // Stateful variable to hold the game that will be displayed to the user
+    const [suggestedGame, setSuggestedGame] = useState({});
+
+    // Stateful variables for the form input values prior to form submission
+    const [platformValue, setPlatformValue] = useState('');
     const [genreValue, setGenreValue] = useState('');
-    // Default to singleplayer games if the user does not choose any tags
     const [tagsValue, setTagsValue] = useState(['singleplayer']);
-    // Stateful variable that triggers the redirect to the game details page - will be set to true on form submit
-    const [toGamePage, setToGamePage] = useState(false);
-    if (toGamePage) {
-        return <Navigate to={`/${props.currentGameId}`} />;
-    }
+        // Default to singleplayer games if the user does not choose any tags
 
-    console.log(props.currentGameId);
+    // Stateful variables for the user's submitted choices
+    const [userPlatform, setUserPlatform] = useState('');
+    const [userGenre, setUserGenre] = useState('');
+    const [userTags, setUserTags] = useState([]);
+
+    // Use useRef to check if it is the page's initial render - if it is, do not make the API call
+    const initialRender = useRef(true);
+
+    // Stateful variable that triggers the redirect to the game details page - will be changed on form submit
+    const [toGamePage, setToGamePage] = useState(false);
+
+    // A function that filters through the data returned by the API and returns games that match the user's parameters
+    const gameFinder = (gamesArray) => {
+        // An array that will hold the results from the first filter (genre)
+        const firstFilter = [];
+        // An array that will hold the final filtered results
+        const finalResults = [];
+
+        // Filter the results for games that match the user's chosen genre
+        gamesArray.forEach((game) => {
+            game.genres.forEach((genre) => {
+                let genreName = genre.slug;
+
+                if (genreName.includes(userGenre)) {
+                    firstFilter.push(game);
+                }
+            });
+        });
+
+        // Filter the results for games that match all of the user's chosen tags
+        firstFilter.forEach((game) => {
+            // An array that will hold the tags for each game so that we can compare them to userTags
+            const tagArray = [];
+
+            // A function that takes two arrays as parameters and checks that targetArray includes every value in checkedArray - returns a boolean value
+            const matchChecker = (targetArray, checkedArray) =>
+                checkedArray.every((value) => targetArray.includes(value));
+
+            game.tags.forEach((tag) => {
+                tagArray.push(tag.slug);
+
+                let isMatch = matchChecker(tagArray, userTags);
+
+                if (isMatch && finalResults.includes(game) === false) {
+                    finalResults.push(game);
+                }
+            });
+        });
+        // After filtering, randomize a result and set it as suggestedGame
+        const arrayRandomizer = (array) => {
+            const arrayIndex = Math.floor(Math.random() * array.length);
+            return array[arrayIndex];
+        };
+        setSuggestedGame(arrayRandomizer(finalResults));
+        setToGamePage(true);
+    };
+
+    // Using useEffect, make an API call once the user has submitted the form
+    useEffect(() => {
+        const apiKey = `92bb52f637714b219136e934ac1b2969`;
+
+        // An array that will hold the total contents of each API call
+        const apiArray = [];
+        // A variable that will be used to check if the next page of API data exists
+        let isNextNull = false;
+
+        // An asynchronous function that will accept a page number as a parameter and make a request to the API using that page number
+        async function getPageOfResults(page) {
+            await axios
+                .get(
+                    `https://api.rawg.io/api/games?key=${apiKey}&page_size=125&ordering=-metacritic&genres=indie&tags=${userTags}&platforms=${userPlatform}&page=${page}`
+                )
+                .then((apiData) => {
+                    if (apiData.data.next == null) {
+                        isNextNull = true;
+                    }
+                    apiData.data.results.forEach((gameObject) => {
+                        apiArray.push(gameObject);
+                    });
+                });
+        }
+
+        // An asynchronous function that will keep track of the current page of API data, whether the next page exists, and make a request for the next page using getPageOfResults() if it does exist
+        async function getResults() {
+            let page = 1;
+            while (apiArray.length < 120 && isNextNull === false) {
+                await getPageOfResults(page);
+                page++;
+            }
+            // Call gameFinder() on the apiArray to filter through the results
+            gameFinder(apiArray);
+        }
+
+        if (initialRender.current) {
+            // If it is the initial render, set initialRender to false
+            initialRender.current = false;
+        } else {
+            // If it isn't the initial render, make the API call
+            getResults();
+        }
+        // Run this side effect when the user makes their choices
+    }, [userPlatform, userTags, userGenre]);
 
     // Change events for each of the inputs
     const changePlatform = (event) => {
         setPlatformValue(event.target.value);
-    }
+    };
 
     const changeGenre = (event) => {
         setGenreValue(event.target.value);
-    }
+    };
 
     // An array to hold the user's checked values that are to be submitted
     const checkedTags = [];
@@ -36,10 +137,10 @@ const Form = (props) => {
                 checkedValues.push(checkbox.value);
             }
         });
-        return checkedValues; 
-    }
+        return checkedValues;
+    };
 
-    // A change event on the checkboxes, which calls getCheckedValues() and pushes its return to the checkedTags array 
+    // A change event on the checkboxes, which calls getCheckedValues() and pushes its return to the checkedTags array
     const changeTags = () => {
         // Run getCheckedValues and store its return in a variable
         const currentlyChecked = getCheckedValues();
@@ -48,173 +149,183 @@ const Form = (props) => {
         // Push each tag within currentlyChecked into the checkedTags array
         currentlyChecked.forEach((tag) => {
             checkedTags.push(tag);
-        })
+        });
         setTagsValue(checkedTags);
     };
 
-    // Submit event for the form 
+    // Submit event for the form
     const formSubmit = (event) => {
         event.preventDefault();
-        props.handleSubmit(platformValue, genreValue, tagsValue);
-        setToGamePage(true);
+        // Store the form values in state
+        setUserPlatform(platformValue);
+        setUserGenre(genreValue);
+        setUserTags(tagsValue);
+    };
+
+    // This works if the user doesn't hit submit too fast
+    while (toGamePage && suggestedGame.id !== undefined) {
+        return <Navigate to={`/${suggestedGame.id}`} />;
     }
 
     return (
-        <form action='' onSubmit={formSubmit}>
-            <h2>What are you looking for?</h2>
+        <div className="form-container">
+            <form action="" onSubmit={formSubmit}>
+                <h2>What are you looking for?</h2>
 
-            <label htmlFor="platform">How do you like to play?</label>
-            <select
-                name="platform"
-                id="platform"
-                onChange={changePlatform}
-                value={platformValue}
-                required
-            >
-                <option value="">Please select a platform</option>
-                <option value="4">PC</option>
-                <option value="7">Nintendo Switch</option>
-                <option value="1">Xbox One</option>
-                <option value="186">Xbox Series S/X</option>
-                <option value="18">PlayStation 4</option>
-                <option value="187">PlayStation 5</option>
-            </select>
+                <label htmlFor="platform">How do you like to play?</label>
+                <select
+                    name="platform"
+                    id="platform"
+                    onChange={changePlatform}
+                    value={platformValue}
+                    required
+                >
+                    <option value="">Please select a platform</option>
+                    <option value="4">PC</option>
+                    <option value="7">Nintendo Switch</option>
+                    <option value="1">Xbox One</option>
+                    <option value="186">Xbox Series S/X</option>
+                    <option value="18">PlayStation 4</option>
+                    <option value="187">PlayStation 5</option>
+                </select>
 
-            <label htmlFor="genre">What type of games do you like?</label>
-            <select
-                name="genre"
-                id="genre"
-                onChange={changeGenre}
-                value={genreValue}
-                required
-            >
-                <option value="">Please select a genre</option>
-                <option value="adventure">Adventure</option>
-                <option value="action">Action</option>
-                <option value="role-playing-games-rpg">RPG</option>
-                <option value="strategy">Strategy</option>
-                <option value="simulation">Simulation</option>
-                <option value="puzzle">Puzzle</option>
-                <option value="platformer">Platformer</option>
-            </select>
+                <label htmlFor="genre">What type of games do you like?</label>
+                <select
+                    name="genre"
+                    id="genre"
+                    onChange={changeGenre}
+                    value={genreValue}
+                    required
+                >
+                    <option value="">Please select a genre</option>
+                    <option value="adventure">Adventure</option>
+                    <option value="action">Action</option>
+                    <option value="role-playing-games-rpg">RPG</option>
+                    <option value="strategy">Strategy</option>
+                    <option value="simulation">Simulation</option>
+                    <option value="puzzle">Puzzle</option>
+                    <option value="platformer">Platformer</option>
+                </select>
 
-            <div className="tags-checkboxes">
-                <p>
-                    Please select another tag you'd like to add to your search.
-                </p>
+                <div className="tags-checkboxes">
+                    <p>
+                        Please select another tag you'd like to add to your
+                        search.
+                    </p>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="singleplayer"
-                    value="singleplayer"
-                    id="singleplayer"
-                    onChange={changeTags}
-                />
-                <label htmlFor="singleplayer">Singleplayer</label>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="singleplayer"
+                        value="singleplayer"
+                        id="singleplayer"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="singleplayer">Singleplayer</label>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="multiplayer"
-                    value="multiplayer"
-                    id="multiplayer"
-                    onChange={changeTags}
-                />
-                <label htmlFor="multiplayer">Multiplayer</label>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="multiplayer"
+                        value="multiplayer"
+                        id="multiplayer"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="multiplayer">Multiplayer</label>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="great-soundtrack"
-                    value="great-soundtrack"
-                    id="great-soundtrack"
-                    onChange={changeTags}
-                />
-                <label htmlFor="great-soundtrack">Great Soundtrack</label>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="great-soundtrack"
+                        value="great-soundtrack"
+                        id="great-soundtrack"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="great-soundtrack">Great Soundtrack</label>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="co-op"
-                    value="co-op"
-                    id="co-op"
-                    onChange={changeTags}
-                />
-                <label htmlFor="co-op">Co-op</label>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="co-op"
+                        value="co-op"
+                        id="co-op"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="co-op">Co-op</label>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="story-rich"
-                    value="story-rich"
-                    id="story-rich"
-                    onChange={changeTags}
-                />
-                <label htmlFor="story-rich">Story Rich</label>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="story-rich"
+                        value="story-rich"
+                        id="story-rich"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="story-rich">Story Rich</label>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="exploration"
-                    value="exploration"
-                    id="exploration"
-                    onChange={changeTags}
-                />
-                <label htmlFor="exploration">Exploration</label>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="exploration"
+                        value="exploration"
+                        id="exploration"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="exploration">Exploration</label>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="family-friendly"
-                    value="family-friendly"
-                    id="family-friendly"
-                    onChange={changeTags}
-                />
-                <label htmlFor="family-friendly">Family Friendly</label>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="family-friendly"
+                        value="family-friendly"
+                        id="family-friendly"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="family-friendly">Family Friendly</label>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="peaceful"
-                    value="peaceful"
-                    id="peaceful"
-                    onChange={changeTags}
-                />
-                <label htmlFor="peaceful">Peaceful</label>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="peaceful"
+                        value="peaceful"
+                        id="peaceful"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="peaceful">Peaceful</label>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="horror"
-                    value="horror"
-                    id="horror"
-                    onChange={changeTags}
-                />
-                <label htmlFor="horror">Horror</label>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="horror"
+                        value="horror"
+                        id="horror"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="horror">Horror</label>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="roguelike"
-                    value="roguelike"
-                    id="roguelike"
-                    onChange={changeTags}
-                />
-                <label htmlFor="roguelike">Roguelike</label>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="roguelike"
+                        value="roguelike"
+                        id="roguelike"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="roguelike">Roguelike</label>
 
-                <input
-                    className="tag-checkbox"
-                    type="checkbox"
-                    name="metroidvania"
-                    value="metroidvania"
-                    id="metroidvania"
-                    onChange={changeTags}
-                />
-                <label htmlFor="metroidvania">Metroidvania</label>
-            </div>
-            <button action="submit">Find a game!</button>
-        </form>
+                    <input
+                        className="tag-checkbox"
+                        type="checkbox"
+                        name="metroidvania"
+                        value="metroidvania"
+                        id="metroidvania"
+                        onChange={changeTags}
+                    />
+                    <label htmlFor="metroidvania">Metroidvania</label>
+                </div>
+                <button action="submit">Find a game!</button>
+            </form>
+        </div>
     );
 }
 
